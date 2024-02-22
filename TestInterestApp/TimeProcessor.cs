394993aeefe43a5614d6a6c7a051a0a56c01b9c5
Bruno Saenz <Brunosaenz@gmail.com>
2024-02-21 23:33:12 -0600

    Added function to determine the unit used for the term length.

--- TestInterestApp/TimeProcessor.cs
+++ TestInterestApp/TimeProcessor.cs
@@ -8,5 +8,23 @@ namespace TestInterestApp
 {
     public class TimeProcessor
     {
+
+        public TimeUnit DetermineTimeUnit(string timeUnit)
+        {
+
+            switch ( timeUnit.ToUpper() )
+            {
+                case "D":
+                    return TimeUnit.Days;
+                case "W":
+                    return TimeUnit.Weeks;
+                case "M":
+                    return TimeUnit.Months;
+                case "Y":
+                    return TimeUnit.Years;
+                default: return TimeUnit.NoUnit;
+
+            }
+        }
     }
 }
