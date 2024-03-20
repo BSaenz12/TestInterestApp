8075b5384c6d6fd12f35750b7a12197782bd205b
Bruno Saenz <Brunosaenz@gmail.com>
2024-03-20 00:30:58 -0500

    Created new function and added comments.

--- TestInterestApp/InterestCalculator.cs
+++ TestInterestApp/InterestCalculator.cs
@@ -6,13 +6,25 @@ using System.Threading.Tasks;
 
 namespace TestInterestApp
 {
-    internal class InterestCalculator
+    public class InterestCalculator
     {
+
+        // This will be the "engine" class that'll be responsible for doing the calculations.
+
+        // to do
+        // 1. add a function that'll calculate the interest rate.
+        public double CalculateInterestRate(TimeUnit  termLengthUnit, double principleAmt, double interestRate, Int64 termLength)
+        {
+            // convert time unit into years since the app takes an APR only
+
+            // calculate the interest rate
+
+            // print out the interest rates and corresponding values
+
+
+            return 0.00;
+        }
+
+        // 2. add a function that'll write out the amounts throughout the term length.
     }
-
-    // This will be the "engine" class that'll be responsible for doing the calculations.
-
-    // to do
-    // 1. add a function that'll calculate the interest rate.
-    // 2. add a function that'll write out the amounts throughout the term length.
 }
